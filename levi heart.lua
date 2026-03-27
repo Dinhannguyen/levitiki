f2cd8edde3494db48813cb98136197529537df55
Dinhannguyen <nguyenquocduynhanaov@gmail.com>
2026-03-27 11:23:04 +0700

    Update levi heart.lua

--- levi heart.lua
+++ levi heart.lua
@@ -74,5 +74,5 @@ else
 end
 
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().Key = "e9162fb60364a89d94d75009"
+getgenv().Key = "1fac947ad1d37c7fc21830fd"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BananaCat-KaitunLevi.lua"))()
